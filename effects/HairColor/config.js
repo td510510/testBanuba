d8fbe6c9c9a589692fbdaa10145eacd089b19a90
td510510 <dangducthinhdang@gmail.com>
2022-06-20 11:00:15 +0700

    add new effect

---
+++ effects/HairColor/config.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const makeup = require("./makeup.js");
+
+Object.assign(globalThis, makeup.m);
+/* Feel free to add your custom code below */
+// set hair color
+Hair.color("0.39 0.14 0.14 0.8")
